5a8f22108adc919fcff2e7cfc6a3fd743524bc47
radua <radua@outlook.com>
2022-05-27 14:29:57 -0400

    Use rate subtotal. Update version number

--- upload/admin/model/extension/shipping/flagship.php
+++ upload/admin/model/extension/shipping/flagship.php
@@ -55,7 +55,7 @@ class ModelExtensionShippingflagship extends Model{
         $url = $this->config->get('smartship_api_url').'/ship/shipments/'.$flagship_shipment_id;
         $token = $this->config->get('shipping_flagship_token');
 
-        $shipment = $this->apiRequest($url,$payload,$token,'PUT',30,'1.0.9',$orderId,$orderLink);
+        $shipment = $this->apiRequest($url,$payload,$token,'PUT',30,'1.0.10',$orderId,$orderLink);
         return $shipment["response"]->content;
     }
 
@@ -147,7 +147,7 @@ class ModelExtensionShippingflagship extends Model{
     public function prepareShipment(array $payload, int $orderId, string $orderLink) : \stdClass {
         $url = $this->config->get('smartship_api_url').'/ship/prepare';
         $token = $this->config->get('shipping_flagship_token');
-        $shipment = $this->apiRequest($url,$payload,$token,'POST',30,'1.0.9',$orderId,$orderLink);
+        $shipment = $this->apiRequest($url,$payload,$token,'POST',30,'1.0.10',$orderId,$orderLink);
         return $shipment["response"]->content;
     }
 
@@ -200,12 +200,12 @@ class ModelExtensionShippingflagship extends Model{
         $url = $this->config->get('smartship_api_url').'/ship/shipments/'.$shipment_id;
         $token = $this->config->get('shipping_flagship_token');
 
-        $shipment = $this->apiRequest($url,[],$token,'GET',10,'1.0.9',$orderId);
+        $shipment = $this->apiRequest($url,[],$token,'GET',10,'1.0.10',$orderId);
         $status = $shipment["response"]->content->status;
         return $status;
     }
 
-    protected function apiRequest(string $url,array $json, string $apiToken,string $method, int $timeout, string $version='1.0.9', int $orderId=0, string $orderLink='') : array {
+    protected function apiRequest(string $url,array $json, string $apiToken,string $method, int $timeout, string $version='1.0.10', int $orderId=0, string $orderLink='') : array {
 
         $curl = curl_init();
         $storeName = $this->config->get('config_name');
